b78e9b139d5edd531dcddad59b9d1c4b373c6998
Mathieu Laflamme <mathieu.laflamme@gmail.com>
2015-09-27 22:28:34 -0400

    Revert "Renamed cz.mlelant.deskclock to ca.mlaflamme.clocktime"
    This reverts commit 02f4b92d4d0f2079d9e2052d267e7e6093670792.

---
+++ ClockPlus/src/main/java/cz/mpelant/deskclock/App.java
@@ -0,0 +1,12 @@
+
+package cz.mpelant.deskclock;
+
+import android.app.Application;
+
+public class App extends Application {
+    public static final String TAG = "App";
+}
+
+//TODO: Do I need to include all Roboto font?
+//TODO: Sizing issue on GS
+//TODO: Icon not there on Nexus 6
